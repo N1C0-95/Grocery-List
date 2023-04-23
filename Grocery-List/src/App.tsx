6e5f1b8de4b2f885b1f1b2e04bd7cb0489e38d9f
Nicolò Ferranti <nico.ferranti95@gmail.com>
2023-04-23 23:00:21 +0200

    add config tailwind, base style, fonts

--- Grocery-List/src/App.tsx
+++ Grocery-List/src/App.tsx
@@ -1,13 +1,16 @@
 function App() {
   return (
     <> 
-    <div className="flex justify-center">
+    <div className="page-sm ">
       
       
       <p className="text-3xl font-bold">
         test font
       </p>
 
+      <button className="btn error">cliccami</button>
+      <button className="btn primary">cliccami</button>
+      <button className="btn success">cliccami</button>
       </div>    
     </>
   )
