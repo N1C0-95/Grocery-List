8dff4e80cee84999eb7d5b8b6be6089b2d6924e1
Nicolò Ferranti <nico.ferranti95@gmail.com>
2023-04-23 16:38:30 +0200

    initial setup

---
+++ Grocery-List/src/App.tsx
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <> 
+    <div className="flex justify-center">
+      
+      
+      <p className="text-3xl font-bold">
+        test font
+      </p>
+
+      </div>    
+    </>
+  )
+}
+
+export default App
