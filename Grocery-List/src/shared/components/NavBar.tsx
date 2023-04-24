79073492e40691d95b2b7266dbea88bb4920a130
Nicolò Ferranti <nico.ferranti95@gmail.com>
2023-04-24 12:21:03 +0200

    manage active navbar

--- Grocery-List/src/shared/components/NavBar.tsx
+++ Grocery-List/src/shared/components/NavBar.tsx
@@ -1,20 +1,28 @@
 import { NavLink } from "react-router-dom";
 import logo from "../../assets/LogoNF-removebg-preview.png";
 
+
 export function NavBar() {
+  
+  
+  function isActive(obj:{isActive:boolean}){
+    return obj.isActive ? "text-fuchsia-500 font-bold" : "text-[#67748E]";
+  }
+  console.log("riprocesso il componente")
   return (
-    <div className="w-full h-16 flex flex-wrap justify-between items-center pr-20 pl-10">
-      
+    
+    <div className="w-full h-24 flex flex-wrap justify-center items-center gap-7
+     sm:h-16 sm:justify-between sm:pr-20 sm:pl-10  ">
       {/* LOGO */}
       <div className="flex ">
         <img src={logo} className="w-12"/>
       </div>
       
       {/* PATH */}
-      <div className="flex items-center gap-10 font-normal text-[#67748E]">
-        <NavLink to={"/home"}>Home</NavLink>
-        <NavLink to={"/grocery"}>Lista</NavLink>
-        <NavLink to={"/pantry"}>Dispensa</NavLink>
+      <div className="flex items-center gap-5 font-normal text-[#67748E]">
+        <NavLink to={"/home"} className={isActive} >Home</NavLink>
+        <NavLink to={"/grocery"} className={isActive} >Lista</NavLink>
+        <NavLink to={"/pantry"} className={isActive}>Dispensa</NavLink>
         <i className="fa fa-user" />
       </div>
     </div>
